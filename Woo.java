f295c823983127d379c2e4b0ab87b436df6d53a0
tangnoah <ntang@stuy.edu>
2017-01-12 10:48:04 -0500

    Worked on menu system for Woo

--- Woo.java
+++ Woo.java
@@ -1,11 +1,27 @@
+import cs1.Keyboard;
+
 public class Woo{
     
     public static void main(String[] args){
-	menu();
+	int choice = 0;
+	while( choice == 0){
+	    clear();
+	    menu();
+	    choice = Keyboard.readInt();
+	    if (choice == 1){ start();}
+	    if (choice == 2){ manual(); choice = 0;}
+	}
+	
     }
 
     public static void start(){
 
+    }
+
+    public static void manual(){
+	clear();
+	System.out.println("MANUAL\nThis is the manual, lorem ipsum etc");
+	Keyboard.readString();
     }
 
     public static void runTurn(){
@@ -13,12 +29,30 @@ public class Woo{
 
     public static void menu(){
 	System.out.println("-=. Historical Military Simulations .=-");
+	System.out.println("Pick one: \n 1.Play Random\n 2.Manual\n");
     }
 
     public static void playerParser(){
     }
 
     public static void clear(){
+	System.out.println("\n");
+	System.out.println("\n");
+	System.out.println("\n");
+	System.out.println("\n");
+	System.out.println("\n");
+	System.out.println("\n");
+	System.out.println("\n");
+	System.out.println("\n");
+	System.out.println("\n");
+	System.out.println("\n");
+	System.out.println("\n");
+	System.out.println("\n");
+	System.out.println("\n");
+	System.out.println("\n");
+	System.out.println("\n");
+	System.out.println("\n");
+	System.out.println("\n");
 	System.out.println("\n");
 	System.out.println("\n");
 	System.out.println("\n");
