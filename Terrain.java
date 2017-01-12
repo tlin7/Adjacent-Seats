2994f3821b380940003c6bfeeab16c68f1de118a
JonathanQuang <jquang@stuy.edu>
2017-01-12 05:37:14 -0500

    Randomise the symbol of terrain for testing purposes

--- Terrain.java
+++ Terrain.java
@@ -3,6 +3,18 @@ public class Terrain{
     protected String symbol="@";
     protected String description;
     
+
+    //Randomizes symbol for testing purposes
+    public Terrain(){
+	int randInt = (int)(Math.random() * 2);
+	if (randInt == 0){
+	    symbol = "@";
+	}
+	else {
+	    symbol = "$";
+	}
+    }
+    
     public String getSymbol(){
 	return symbol;
     }
