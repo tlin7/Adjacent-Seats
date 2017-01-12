784e1bab9ede0aa5fdec33b09dee7235fc5de6fa
tlin7 <tlin7@stuy.edu>
2017-01-12 12:25:27 -0500

    Removed uncessary lines. Changed Water to Sea.

--- Land.java
+++ Land.java
@@ -1,7 +1,5 @@
 public class Land extends Terrain{
     public Land(){
-	String name = "";
-	String symbol = "@";
 	String description = "This is solid ground. Infantry can cross, but Navy cant.";
     }
 }
--- Sea.java
+++ Sea.java
@@ -1,7 +1,5 @@
-public class Water extends Terrain{
-    public Water(){
-	String name = "";
-	String symbol = "~";
+public class Sea extends Terrain{
+    public Sea(){
 	String description = "This is a liquid filled area. Navy can cross, but Infantry cannot.";
     }	
 }
