b92a32fb0fbb2fca2644192d35c3954bab5fd318
Thomas Lin <tlin7@stuy.edu>
2017-01-12 11:45:03 -0500

    Unit subclasses

---
+++ Infantry.java
@@ -0,0 +1,9 @@
+public class Infantry extends Unit{
+    public Infantry(){
+	int strength = 10;
+	int weapon = 10;
+	String[]viableTerrain = "";
+	int attackRange = 10;
+	int moveRange = 1;
+    }
+}
