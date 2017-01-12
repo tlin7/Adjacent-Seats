6f8286c7550db99bd9d2e477dcdee3f311be5942
BuildTools <ntang@stuy.edu>
2017-01-12 18:45:48 -0500

    Implemented basic generation of random battlefield.

--- Woo.java
+++ Woo.java
@@ -1,8 +1,14 @@
 import cs1.Keyboard;
 
 public class Woo{
+
+    //contains the FIELD of TERRAIN tiles
+    public static Terrain[][] field;
+    public static Unit[][] units;
     
+    //main
     public static void main(String[] args){
+	field = new Terrain[20][20];
 	int choice = 0;
 	while( choice == 0){
 	    clear();
@@ -14,14 +20,27 @@ public class Woo{
 	
     }
 
+    //intializes a new random field and some new random units
     public static void start(){
-
+	
+	//generate a random field
+	for( int x = 0; x < field.length; x++){
+	    for( int y = 0; y < field.length; y++){
+		int randchoice = (int)(Math.random()*2); //<== INSERT number of choices
+		if ( randchoice == 0)
+		    field[x][y] = new Land();
+		if ( randchoice == 1)
+		    field[x][y] = new Sea();
+		//if (randchoice == 2)
+	    }
+	}
     }
 
     public static void manual(){
 	clear();
-	System.out.println("MANUAL\nThis is the manual, lorem ipsum etc");
-	Keyboard.readString();
+	System.out.println("MANUAL\nEnter anything to close the manual.");
+
+	Keyboard.readStringAny();
     }
 
     public static void runTurn(){
