5dafdaa0c486d011a66e487040e6df7e047d397f
tangnoah <ntang@stuy.edu>
2017-01-17 10:48:00 -0500

    Added scenario class

---
+++ Scenarios.java
@@ -0,0 +1,44 @@
+public class Scenarios{
+
+    //This is Scenarios.java, the class with all the premade
+    //scenarios
+    
+    //for now, each scenario is just hard coded in this file.
+
+    /*
+    public static void <battlename>(String[] playerNames, 
+    Terrain[][] field, Unit[][] units){
+				
+    field = new Terrain[ <number> ][ <number> ];
+    units = new Unit   [ <number> ][ <number> ];
+
+    //Area to add terrain, units
+
+
+    }
+    */
+
+    public static void hastings(String[] playerNames, 
+				    Terrain[][] field, Unit[][] units){
+	
+	field = new Terrain[ 15 ][ 15 ];
+	units = new Unit   [ 15 ][ 15 ];
+
+	//Area to add terrain, units
+	field = {
+	    {new Land(), new Land()}
+
+	}
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
