c8eeed6bab65585db1ca640c0e341666971fb5ae
tangnoah <ntang@stuy.edu>
2017-01-18 10:51:08 -0500

    minor changes to scenarios

--- Scenarios.java
+++ Scenarios.java
@@ -9,8 +9,7 @@ public class Scenarios{
     public static void <battlename>(String[] playerNames, 
     Terrain[][] field, Unit[][] units){
 				
-    field = new Terrain[ <number> ][ <number> ];
-    units = new Unit   [ <number> ][ <number> ];
+    String 
 
     //Area to add terrain, units
 
@@ -18,18 +17,49 @@ public class Scenarios{
     }
     */
 
-    public static void hastings(String[] playerNames, 
-				    Terrain[][] field, Unit[][] units){
-	
-	field = new Terrain[ 15 ][ 15 ];
-	units = new Unit   [ 15 ][ 15 ];
-
-	//Area to add terrain, units
-	field = {
-	    {new Land(), new Land()}
-
+    public static void stringTofield(String prefield, Terrain[][] field ){
+	int height = 0;
+	int width = 0;
+	//SET HEIGHT AND WIDTH OF NEW ARRAY BY SEARCHING
+	// FOR NEWLINES IN input string
+	for(int x = 0; x < prefield.length() ; x++){
+	    if ( prefield.substring(x,x+1).equals('\n')){
+		height += 1;
+	    }
 	}
-    }
+	for(int x = 0; x < prefield.length(); x++){
+	    if ( prefield.substring(x,x+1).equals('\n')){
+		width = x + 1;
+		break;}
+	}
+	//==============================================
+
+	field = new Terrain[height][width];
+
+	//ADD an OBJECT for each corresponding character in the STRING
+	int y = 0;
+	for( int x = 0; x < prefield.length(); x++){
+	    if( prefield.substring(x,x+1).equals('\n'))
+		y+=1;
+	    if( prefield.substring(x,x+1).equals('-') ) // - IS LAND
+		field[y][x % width] =new Land();
+	    //add more conditionals
+	}}
+
+	public static void test(Terrain[][] field, Unit[][] units){
+	    String prefield = "-----\n-----\n-----\n-----\n-----";
+	    stringTofield(prefield, field);
+	}
+
+	public static void main( String[] args){
+	    Terrain[][] field;
+	    Unit[] units;
+	    test(field, units);
+	}
+	
+
+
+
 
 
 
